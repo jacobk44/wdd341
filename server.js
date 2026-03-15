e64306a4f88c6824538013dccf7e38bd698cffbc
jacobk44 <jacobkwartengamoah@gmail.com>
2026-03-15 12:34:13 -0700

    fix my code

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Z-Key',
   );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTION');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTION',
+  );
   next();
 });
 
